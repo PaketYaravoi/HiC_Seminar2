2a5694b2abc3a01d265574a920bbf0a18ae09ff9
GroshkovR <rik1810huragan@gmail.com>
2024-07-05 15:47:15 +1100

    Создал вводную програму занятия: Найти максимальную цифру из двухзначного числа

---
+++ input/Program.cs
@@ -0,0 +1,16 @@
+﻿//int a = 0; 
+
+//int MyFunction();  // создание метода
+
+//int MyFunctions(int arg1,int arg2)  // создание метода с аргументами , которая ничего не возращает void , она только печатает
+//{
+//    Console.WriteLine($"1 - {arg1} and {arg2}");
+//}
+
+//Приветственная функция:
+
+void Hello(string name = "Roman")
+{
+    Console.WriteLine($" Hello {name} !");
+}
+Hello();
---
+++ tesk1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая выводит случайное число их отрезка [10, 99] и показывает наибольшею цифру числа:
+
+//int GetRandomNum()
+//{
+//    int resolt = new Random().Next(10, 100);
+//    return resolt;
+//}
+
+//int resolt = GetRandomNum();
+//Console.WriteLine(resolt);
+
+//int ShowMaxDigit(int number)
+//{
+//    if (number >= 10)
+//    { 
+//        number = 
+//    }
+//}
+
+
+int GetRandomNumber(int minValue = 10, int maxValue = 99)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+
+
+int GetMaxTwoDigit(int number)
+{
+    int digit1 = number / 10;
+    int digit2 = number % 10;
+    if (digit1 > digit2) return digit1;
+    else return digit2;
+}
+
+int number = GetRandomNumber();
+Console.WriteLine(number);
+int maxDigit = GetMaxTwoDigit(number);
+Console.WriteLine(maxDigit);
+ 
