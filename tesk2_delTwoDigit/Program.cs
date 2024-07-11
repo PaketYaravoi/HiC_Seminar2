78652f343224347cc6c0106ccad72e65d54b1752
GroshkovR <rik1810huragan@gmail.com>
2024-07-11 15:01:37 +1100

    написал 2 программы в рамках семинара (tesk1 and tesk2)

---
+++ tesk2_delTwoDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу , которая выдает случайное трехзначное число и удаляет вторую цифру этого числа.
+
+
+int GetRandomNum(int maxValue, int minValue)
+{
+   int number = new Random().Next(maxValue, minValue);
+    return number;
+}
+
+int GetNewNumber(int number)
+{
+    int number1 = number / 100;
+    int number2 = number % 10;
+    int numberNew = (number1 * 10) + number2;
+    return numberNew;
+}
+
+int number = GetRandomNum(100, 1000);
+Console.WriteLine(number);
+
+int otherNum = GetNewNumber(number);
+Console.WriteLine(otherNum);
+
+
+
