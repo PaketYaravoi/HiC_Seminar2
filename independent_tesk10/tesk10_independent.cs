b11314bcfb6971ffe31f04e0fea1d7e1a473298c
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-07-12 14:33:56 +1100

    Написал программу поиска нужных цыфр: самостоятельная работа 10 и 13

---
+++ independent_tesk10/tesk10_independent.cs
@@ -0,0 +1,29 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+int GetRandomNum(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue);
+        
+}
+
+int GetRigthNumber(int number)
+{
+    while (number >= 100)
+    {
+        number /= 10;
+    }
+    return number %= 10;
+}
+
+
+int number = GetRandomNum(100,  1000);
+Console.WriteLine(number);
+
+int answer = GetRigthNumber(number);
+Console.WriteLine(answer);
+
---
+++ independnt_tesk13/test13.cs
@@ -0,0 +1,55 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+int number =0;
+Console.Write("Enter integer: ");
+
+
+while (!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.WriteLine("input error!");
+}
+
+
+int GetRigthNumber(int number)
+{
+    while (number >= 1000)
+    {
+        number /= 10;
+    }
+    return number;
+}
+
+int GetRigthDigit(int number)
+{
+    while (number >= 10)
+    {
+        number %= 10;
+    }
+    return number;
+}
+
+
+if (number > 100)
+{
+    int rigthNumber = GetRigthNumber(number);
+    int answer = GetRigthDigit(rigthNumber);
+    Console.WriteLine($"third digit = {answer}");
+}
+else
+{
+    Console.WriteLine("no third number");
+}
+
+
+
+
+
+
+
+
